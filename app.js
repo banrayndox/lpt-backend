4f1e1e99790a14baa36da20ecefc3739b42fb924
banrayndox <banrayndox@gmail.com>
2026-06-23 00:17:49 +0600

    Update app.js

--- app.js
+++ app.js
@@ -11,13 +11,23 @@ import apiRoutes from './routes/api.js';
 const app = express();
 
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://diulab.vercel.app",
+];
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173", process.env.CLIENT_URL   ],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
   })
 );
 app.use(express.json());
